3ad6c949f0e418093c842ece5a2733c4906022f8
unknown <aryanmishr291@gmail.com>
2026-05-01 23:57:34 +0530

    new

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://mern-task-manager-app.netlify.app", "http://localhost:3000"],
+    origin: ["task-manager-etharaai.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
